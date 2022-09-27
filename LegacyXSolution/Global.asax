a1f0c6b49c990f093df4db091303cb89b96b98ad
rkelly30 <46577129+rkelly30@users.noreply.github.com>
2022-09-26 20:02:51 -0600

    Add project files.

---
+++ LegacyXSolution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LegacyXSolution.Global" Language="C#" %>
---
+++ LegacyXSystem.Data/Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyXSystem.Data
+{
+    public class Class1
+    {
+    }
+}
